94389946fcd4b2c7d486a68c78cc31cd097cee76
crisber <bcbs1986@gmail.com>
2026-01-18 00:38:33 -0300

    Se modifica

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { CrearProducto } from './components/admin/crear-producto/crear-producto'
 import { Checkout } from './components/checkout/checkout';
 import { AdminDashboard } from './pages/admin-dashboard/admin-dashboard';
 import { authGuard } from './guards/auth-guard';
+import { Register } from './components/register/register';
 
 export const routes: Routes = [
     { path: '', component: CatalogComponent },
@@ -20,5 +21,6 @@ export const routes: Routes = [
         component: AdminDashboard,
         canActivate: [authGuard] // ¡Importante protegerla!
         // Idealmente crearías un adminGuard que verifique el rol
-    }
+    },
+    { path: 'registro', component: Register },
 ];

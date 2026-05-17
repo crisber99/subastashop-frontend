75fa5cfdc419297def082840e0993c680e33cd77
crisber <bcbs1986@gmail.com>
2026-05-17 00:15:30 -0400

    fix: disable launchMode to show promotion banner

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@ export const environment = {
   // La URL de WebSockets en Azure
   wsUrl: 'https://api-subastashop-dhd5gec8hecxfbc9.centralus-01.azurewebsites.net/ws-subastas',
   mercadopagoPublicKey: 'APP_USR-fb4368f5-1a12-43ad-b6f0-291e2b834ec2',
-  launchMode: true
+  launchMode: false
 };

2f92f6ae519c9baa67d9b669c562c4da69f17b02
crisber <bcbs1986@gmail.com>
2026-05-16 23:04:29 -0400

    fix: corregir ruta de importacion de environment en landing-teaser

--- src/app/components/landing-teaser/landing-teaser.ts
+++ src/app/components/landing-teaser/landing-teaser.ts
@@ -2,7 +2,7 @@ import { Component, inject, signal, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { environment } from '../../../../environments/environment';
+import { environment } from '../../../environments/environment';
 import Swal from 'sweetalert2';
 import { AuthService } from '../../services/auth-service';
 
